43bc45b118a4b41c402f529e18141ffa1fd4d1cd
Mouhib777 <mouhibsahbani@gmail.com>
2023-12-09 18:04:59 -0500

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/constants/constant.dart';
 import 'package:flutter_application_4/firebase_options.dart';
 import 'package:flutter_application_4/screens/SplashScreen.dart';
 
@@ -18,14 +19,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color myColor = color1;
+
+    MaterialColor myThemeColor = MaterialColor(
+      myColor.value,
+      <int, Color>{
+        50: myColor.withOpacity(0.1), // Define shades for the color (50 to 900)
+        100: myColor.withOpacity(0.2),
+        200: myColor.withOpacity(0.3),
+        300: myColor.withOpacity(0.4),
+        400: myColor.withOpacity(0.5),
+        500: myColor.withOpacity(0.6),
+        600: myColor.withOpacity(0.7),
+        700: myColor.withOpacity(0.8),
+        800: myColor.withOpacity(0.9),
+        900: myColor.withOpacity(1.0),
+      },
+    );
+    ThemeData myTheme = ThemeData(
+      primarySwatch: myThemeColor,
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Mou7iti',
+      theme: myTheme,
       home: SplashScreen(),
     );
   }
