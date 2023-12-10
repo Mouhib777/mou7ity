70e3bc304f0ecc2189debdcdf21c63332f13a3b5
Mouhib777 <mouhibsahbani@gmail.com>
2023-12-10 05:34:22 -0500

    c

--- lib/screens/NavBar/sensors.dart
+++ lib/screens/NavBar/sensors.dart
@@ -38,16 +38,16 @@ class _SensorsState extends State<Sensors> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Container(
-                    height: screenWidth * 0.3,
-                    width: screenWidth * 0.3,
+                    height: screenWidth * 0.35,
+                    width: screenWidth * 0.35,
                     decoration: BoxDecoration(
                         color: color1,
                         borderRadius: BorderRadius.circular(15),
                         border: Border.all(color: Colors.black)),
                   ), 
                     Container(
-                    height: screenWidth * 0.3,
-                    width: screenWidth * 0.3,
+                    height: screenWidth * 0.35,
+                    width: screenWidth * 0.35,
                     decoration: BoxDecoration(
                         color: color1,
                         borderRadius: BorderRadius.circular(15),
@@ -55,7 +55,57 @@ class _SensorsState extends State<Sensors> {
                   ), 
 
                 ],
-              )
+                
+              ), 
+              SizedBox(height: screenHeight*0.09,),
+                   Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: screenWidth * 0.35,
+                    width: screenWidth * 0.35,
+                    decoration: BoxDecoration(
+                        color: color1,
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: Colors.black)),
+                  ), 
+                    Container(
+                    height: screenWidth * 0.35,
+                    width: screenWidth * 0.35,
+                    decoration: BoxDecoration(
+                        color: color1,
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: Colors.black)),
+                  ), 
+
+                ],
+                
+              ), 
+              SizedBox(height: screenHeight*0.09,),
+                   Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: screenWidth * 0.35,
+                    width: screenWidth * 0.35,
+                    decoration: BoxDecoration(
+                        color: color1,
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: Colors.black)),
+                  ), 
+                    Container(
+                    height: screenWidth * 0.35,
+                    width: screenWidth * 0.35,
+                    decoration: BoxDecoration(
+                        color: color1,
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: Colors.black)),
+                  ), 
+
+                ],
+                
+              ), 
+              
                 ],
               ),
         ),
