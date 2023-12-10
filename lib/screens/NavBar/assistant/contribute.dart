6a73cd1d58b7c8f50782dcc6d58a605a45fd3c5d
Mouhib777 <mouhibsahbani@gmail.com>
2023-12-10 09:16:40 -0500

    c

---
+++ lib/screens/NavBar/assistant/contribute.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Contribute extends StatefulWidget {
+  const Contribute({super.key});
+
+  @override
+  State<Contribute> createState() => _ContributeState();
+}
+
+class _ContributeState extends State<Contribute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+    );
+  }
+}
