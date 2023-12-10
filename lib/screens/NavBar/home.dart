77be47742ae5936b2f2a294536d6e00e7ab12313
Mouhib777 <mouhibsahbani@gmail.com>
2023-12-10 00:52:27 -0500

    c

--- lib/screens/NavBar/home.dart
+++ lib/screens/NavBar/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/constants/constant.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Mou7iti",
+          style: GoogleFonts.montserrat(
+              fontSize: 25, color: color1, fontWeight: FontWeight.w600),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
   }
 }
